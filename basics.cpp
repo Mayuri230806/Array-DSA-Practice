2e9ccac08733ce7009d173d52a1464ed1be1aaac
Mayuri230806 <mayurimangal79@gmail.com>
2026-06-17 22:05:05 +0530

    Add files via upload

---
+++ basics.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+#include <algorithm>
+#include<unordered_map>
+using namespace std;
+
+
+
+// int main(){
+//   int arr[]={2,3,5,7,8};
+//   int n=5;      //previous three line same in code for upadte delete and insert
+//    arr[2]=7;    //update the vaue in array 
+//   for(int i=0;i<n;i++){
+//     cout<<arr[i];
+//   }
+// }
+
+
+// int pos=1;
+// int val=9;
+
+// for(int i=n;i>pos;i--){
+//   arr[i]=arr[i-1];      //inset the element in between the array
+// }
+// arr[pos]=val;
+//   n++;
+// for(int i=0;i<n;i++){
+// cout<<arr[i];
+// }
+
+
+
+
+// int pos=3;
+// int val=2;
+
+// for(int i=n;i>pos;i--){
+//  arr[i]=arr[i-1];
+
+// }
+// arr[pos]=val;  //insert  the value at middle in the array 
+// n++;
+
+// for(int i=0;i<n;i++){
+//   cout<<arr[i];
+// }
+
+
+
+
+
+// int pos=5;
+// int val=9;
+
+//  arr[pos]=val;
+//   n++;
+//                           //  insert the val in last
+// for(int i=0;i<n;i++){
+//      cout<<arr[i];
+//    }
+
+
+
+
+
+// int pos=1;
+// for(int i=pos;i<n-1;i++){
+//   arr[i]=arr[i+1];
+// }
+// n--;
+
+// for(int i=0;i<n;i++){
+//   cout<<arr[i];       //delete
+// }
+// }
+
+
+
